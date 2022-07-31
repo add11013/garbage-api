f5a6d272f3b383c813a1f72ee176837a5b86fecc
add11013 <j8888888871@yahoo.com.tw>
2022-07-31 22:57:48 +0800

    change title and color

--- src/App.vue
+++ src/App.vue
@@ -9,8 +9,4 @@ import 'openapi-explorer';
 </template>
 
 <style>
-    /* <<< targets navigation bar part selector, it's called `section-navbar`. We can see from the image that there is a linear gradient applied to using the original nav background color, transitioned to black. */
-    openapi-explorer::part(section-navbar) {
-      background: linear-gradient(90deg, var(--nav-bg-color), black);
-    }
-  </style>
+</style>
